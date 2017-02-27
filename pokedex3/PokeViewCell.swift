7561c9ab44c3cba51362ccc40b22673b0c97c3d3
AlfredoMarrero <a.e.marrero@hotmail.com>
2017-02-27 13:47:23 -0500

    Add collection view and view cell controller

---
+++ pokedex3/PokeViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokeViewCell.swift
+//  pokedex3
+//
+//  Created by Alfredo M. on 2/27/17.
+//  Copyright © 2017 Alfredo M. All rights reserved.
+//
+
+import UIKit
+
+class PokeViewCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+    
+        self.pokemon = pokemon
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImage.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
