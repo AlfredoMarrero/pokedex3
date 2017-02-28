779ef7eaa0aa30e6f8778b96c6ea8cf05c248bbb
AlfredoMarrero <a.e.marrero@hotmail.com>
2017-02-28 08:22:18 -0500

    Add IBOutlets and model changes.

--- pokedex3/PokemonDetailVC.swift
+++ pokedex3/PokemonDetailVC.swift
@@ -13,10 +13,34 @@ class PokemonDetailVC: UIViewController {
     var pokemon: Pokemon!
     
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+
+    
+    
+    
+    
+    
+    //@IBOutlet weak var nameLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        nameLbl.text = pokemon .name
 
     }
+
+    @IBAction func backButton(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+
 }
